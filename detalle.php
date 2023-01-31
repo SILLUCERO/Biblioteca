36aaef34677773010d95d5340901530048f1f2f5
NataliaVorobyeva <nataliavorobyeva5@gmail.com>
2023-01-31 11:57:39 +0100

    estilos detalle

--- detalle.php
+++ detalle.php
@@ -12,6 +12,7 @@ $query = mysqli_query($con, $sql);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilos/style.css" />
     <link rel="stylesheet" href="./detalle.css" />
     <title>Biblioteca</title>
 </head>
@@ -21,18 +22,24 @@ $query = mysqli_query($con, $sql);
     </header>
     <?php
     while($row = mysqli_fetch_array($query)):?>
-    <div class='container'>
-        <h2><?=$row['titulo']?></h2>
-        <p><?=$row['ISBN']?></p>
-        <p><?=$row['autor']?></p>
-        <p><?=$row['descripcion']?></p>
-        <img src=<?=$row['portada']?>>
+    <div class="container1">
+            <div class="container2">
+                <img class="portada" src=<?=$row['portada']?>>
+            </div>
+        <main class="container3">
+            <div class='container4'>
+                <h2><?=$row['titulo']?></h2>
+                <p><?=$row['ISBN']?></p>
+                <p><?=$row['autor']?></p>
+                <p><?=$row['descripcion']?></p>
+            </div>
 
-         <div> 
-           <a href="edicion-formulario.php?id=<?= $row['id'] ?>">Editar</a>
-           <a href="./logica/borrar.php?id=<?= $row['id'] ?>">Borrar</a>
-         </div>
-         
+            <div class="btnIcons"> 
+                <a class="editar" href="edicion-formulario.php?id=<?= $row['id'] ?>">Editar</a>
+                <a class="borrar" href="./logica/borrar.php?id=<?= $row['id'] ?>">Borrar</a>
+            </div>
+            
+        </main>
     </div>
     <?php endwhile;?>
 </body>
