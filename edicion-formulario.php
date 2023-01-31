06a4a5ea9cc9fe16cd18e140da8c8d73607ed392
LuisaVAZ <lilo_va@hotmail.com>
2023-01-31 14:56:14 +0100

    Contraseñas

--- edicion-formulario.php
+++ edicion-formulario.php
@@ -22,9 +22,9 @@ $row = mysqli_fetch_array($query);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link rel="stylesheet" type="text/css" href="http://localhost/Biblioteca/estilos/style.css"> -->
+    <link rel="stylesheet" type="text/css" href="http://localhost/Biblioteca/estilos/style.css">
 
-    <link rel="stylesheet" type="text/css" href="http://localhost:8888/Biblioteca/estilos/style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="http://localhost:8888/Biblioteca/estilos/style.css"> -->
 
 </head>
 
--- formulario.php
+++ formulario.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca</title>
-    <!-- <link rel="stylesheet" type="text/css" href="http://localhost/Biblioteca/estilos/style.css"> -->
-    <link rel="stylesheet" type="text/css" href="http://localhost:8888/Biblioteca/estilos/style.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/Biblioteca/estilos/style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="http://localhost:8888/Biblioteca/estilos/style.css"> -->
 </head>
 <body>
     <div class="box0">
