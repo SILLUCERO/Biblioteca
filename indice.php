9df082921a28f566b4ea9c044b4ad861ad974c67
Little Z <zy_mz17@hotmail.es>
2023-02-02 09:05:42 +0100

    cambio a mampp

--- indice.php
+++ indice.php
@@ -19,8 +19,8 @@ $query = mysqli_query($con1, $SQL_READ);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" type="text/css" href="http://localhost/Biblioteca/estilos/style.css"> -->
-    <link rel="stylesheet" type="text/css" href="http://localhost:8888/Biblioteca/estilos/style.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/Biblioteca/estilos/style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="http://localhost:8888/Biblioteca/estilos/style.css"> -->
 
     <title>Biblioteca</title>
 </head>
