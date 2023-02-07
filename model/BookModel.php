da265b93ec51268bbbc23b88ac313c567597ac6b
SILLUCERO <silvinalaura@outlook.es>
2023-02-07 14:51:54 +0100

    create

--- model/BookModel.php
+++ model/BookModel.php
@@ -3,7 +3,7 @@
 class BookModel
 {
     public $conn;
-
+    public $table ="libros";
     public function __construct()
     {
         require_once("/Applications/MAMP/htdocs/Biblioteca2/config/Database.php");
@@ -17,4 +17,16 @@ class BookModel
         $query = $this->conn->query('SELECT * FROM libros');
         return $query->fetch_all(MYSQLI_ASSOC);
     }
+    public function createBooks()
+    {
+    $isbn = $_POST['isbn'];
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $descripcion = $_POST['descripcion'];
+    $portada = $_POST['portada'];
+        $query = $this->conn->query("INSERT INTO".$this->table." (`id`, `ISBN`, `titulo`, `autor`, `descripcion`, `portada`) 
+        VALUES (NULL, '$isbn', '$titulo', '$autor', '$descripcion', '$portada')");
+        return $query->fetch_all(MYSQLI_ASSOC);
+    }
 }
+
