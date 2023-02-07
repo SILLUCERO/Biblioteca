6745491dd289e5fc55e4eeebae149e4f9430e682
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-02-07 12:45:56 +0100

    primer update estilos

--- view/index.php
+++ view/index.php
@@ -13,7 +13,7 @@ $result = $controller->getBooks();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" type="text/css" href="http://localhost/Biblioteca/estilos/style.css"> -->
-    <link rel="stylesheet" type="text/css" href="http://localhost:8888/Biblioteca2/estilos/style.css">
+    <link rel="stylesheet" type="text/css" href="../estilos/style.css">
     <title>Biblioteca2</title>
 </head>
 
