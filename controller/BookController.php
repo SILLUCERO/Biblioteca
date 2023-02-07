119ff7ee3184b6301ccd28660a6f03235719d449
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-02-07 11:02:52 +0100

    cambios en BookController

--- controller/BookController.php
+++ controller/BookController.php
@@ -1,5 +1,21 @@
 <?php
 
+class MemberController
+{
+    public $model;
+
+    public function __construct()
+    {
+        require_once("/Applications/MAMP/htdocs/MVC_EXERCISE/model/MemberModel.php");
+        $this->model = new MemberModel();
+    }
+    //pregunta al modelo si ya ha obtenido los datos de la db
+    public function getMembers()
+    {
+        return ($this->model->getMembers() ? $this->model->getMembers() : "there is no members");
+
+    }
+}
 
 
 ?>
