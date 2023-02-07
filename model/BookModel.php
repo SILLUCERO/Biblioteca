245a4034370166a3b4fb17fc2987e9c569516262
juliana montano <zjulim89@gmail.com>
2023-02-07 10:19:03 +0100

    creacion de pagina bookmodel

---
+++ model/BookModel.php
@@ -0,0 +1,20 @@
+<?php
+
+class BookModel
+{
+    public $conn;
+
+    public function __construct()
+    {
+        require_once("/Applications/MAMP/htdocs/Biblioteca2/config/Database.php");
+        //require_once("C:/xampp/htdocs/Biblioteca2/config/Database.php");
+        $db = new Database();
+        $this->conn = $db->connection();
+    }
+
+    public function getBooks()
+    {
+        $query = $this->conn->query('SELECT * FROM libros');
+        return $query->fetch_all(MYSQLI_ASSOC);
+    }
+}
